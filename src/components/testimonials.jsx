5795c808b9b9e9420bbe487f555da44ef148875f
ashirwadtextiles <ashirwadtextiles@gmail.com>
2023-10-22 11:31:41 +0530

    fixed testimonial responsive

--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -5,7 +5,7 @@ export const Testimonials = (props) => {
     <div id="testimonials">
       <div className="container">
         <div className="section-title text-center">
-          <h2>Testimonials</h2>
+        <h2 style={{ position: "relative", left: "10px" }}>Testimonials</h2>
         </div>
         <div className="row">
           {props.data
